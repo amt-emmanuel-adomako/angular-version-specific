1e13cdcef0cd49a95e73defe8b83bcc123c30123
ecy20 <e.adomako@amilitech.org>
2023-11-26 13:13:59 -0800

    initial commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'angular-verion-specific';
+  title = 'Hello world!';
 }
