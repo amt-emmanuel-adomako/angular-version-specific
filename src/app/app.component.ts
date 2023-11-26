00de669d11829042ebebeff8ddc139bac182a3d8
ecy20 <e.adomako@amilitech.org>
2023-11-26 15:27:45 -0800

    initial commit 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Hello world!';
+  @ViewChild('docName') docname: ElementRef;
   document_type = document.getElementById('documentType')
 
   constructor(){}
   
+  docNameChange(element){
+    console.log(element.target.value.length);
+  }
+  emailChange(element){
+    console.log(element.target.value.length);
+  }
+
+  categoryChange(element){
+    console.log(element.target.value);
+  }
+
+  docTypeChange(element){
+    console.log(element.target.value);
+  }
+  ngOnInit(): void {
+    
+  }
 }
